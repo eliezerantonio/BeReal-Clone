6b7cdf7fc2277b0b83553b129647f43154a98098
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-05-27 04:31:40 +0100

    feat(✨): Camera implementation

---
+++ BeRealClone/Shared/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  BeRealClone
+//
+//  Created by Eliezer Antonio on 27/05/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
---
+++ BeRealClone/Shared/CameraViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CameraViewModel.swift
+//  BeRealClone
+//
+//  Created by Eliezer Antonio on 27/05/24.
+//
+
+import Foundation
